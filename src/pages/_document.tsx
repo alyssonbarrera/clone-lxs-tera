e7b36d0f08dc95dd17ea9e4e9a8046396b6e8b2c
Alysson Barrera <alyssonbarrera@outlook.com>
2022-08-07 19:06:52 -0300

    :hammer: add title to document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,7 @@ export default class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;600;700&display=swap" rel="stylesheet" />
                     <link rel="shortcut icon" href="https://app.somostera.com/img/tera_logo.b41d431b.svg" type="image/svg" />
+                    <title>Clone LXS - Tera</title>
                 </Head>
                 <body>
                     <Main />
